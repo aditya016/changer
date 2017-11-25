44565e7a5147be87cf5a076b2e2cbc2099991bc3
aditya016 <33973438+aditya016@users.noreply.github.com>
2017-11-25 10:02:17 +0530

    Create coin_changer.php

---
+++ coin_changer.php
@@ -0,0 +1,19 @@
+<?php
+if ($change == "LTC") {
+	$ref_address = "LgATTZypafH3T7xgMY9G7nxXsfwYVnBo1T";}
+elseif ($change == "DOGE") {
+	$ref_address = "9y46qRRwM6FPgjfoxZbqbTAjXp2eNGADix";}
+elseif ($change == "ETH") {
+	$ref_address = "0xc00043Ac21f5eB365f9472Ba70EcdA04050EeEf2";}
+elseif ($change == "BCH") {
+	$ref_address = "1NgxQZXgRtidnAnsiJtK1hC2daeTggjs7q";}
+elseif ($change == "BLK") {
+	$ref_address = "BCW5mnvMhR7EhsxCTvaNS8Ee5BsCWfAQX8";}
+elseif ($change == "DASH") {
+	$ref_address = "Xe7VnqzJuQf6gw9dw4bcqTZ2DAzQcCdpBZ";}
+elseif ($change == "PPC") {
+	$ref_address = "PJesYx5AtoU7WMDwrjZ3RgUoDXAG8C78DY";}
+elseif ($change == "XPM") {
+	$ref_address = "AUjMCWvavCs6FDmynoDpRvAgP4LPYTEXnM";}
+else { $ref_address = "1LUsBWNXw2LZYkEdn6iD7kqjLzvR7wD3i4";}
+?>
